3e7263e4d6e4c56dbdda5a3f6ca145398dfdd877
Vinay Wasnik <127746998+VinayWasnik@users.noreply.github.com>
2023-09-14 19:15:05 +0530

    Update MealsProvider.js

--- app/MealsProvider.js
+++ app/MealsProvider.js
@@ -6,17 +6,25 @@ const todaysMeals = [
     { id: 2, name: 'Baked Sweet Potatoes' },
     { id: 3, name: 'Baked Potatoes' },
 ]
+/*started here*/
+const MealsContext = createContext();
 
-const MealsProvider = () => {
+const MealsProvider = ({ children }) => {
     const [meals, setMeals] = useState(todaysMeals);
 
-    const tickMeal = () => {
-
+    const tickMeal = (id) => {
+        setMeals(prevMeals =>
+            prevMeals.map(meal =>
+                meal.id === id ? { ...meal, ticked: !meal.ticked } : meal
+            )
+        );
     }
 
     return (
         <div>
-            
+            <MealsContext.Provider value={{ meals, tickMeal }}>
+                {children}
+            </MealsContext.Provider>
         </div>
     )
 };
